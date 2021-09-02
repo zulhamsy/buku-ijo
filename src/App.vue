b4f23fb6f0ded50d63564619956cfcc90ec08e4f
Zulham Syafrawi <official.zulham@gmail.com>
2021-09-02 10:30:47 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <test-page />
+  </div>
+</template>
+
+<script>
+import TestPage from './views/LoginPage.vue'
+export default {
+  components: {
+    testPage: TestPage
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  padding: 0 1.5rem;
+}
+</style>
---
+++ src/views/LoginPage.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="d-flex flex-column justify-content-center min-vh-100">
+    <!-- App Title -->
+    <div class="d-flex align-items-center">
+      <img
+        src="..\assets\brand-logo.svg"
+        alt="brand logo"
+        class="logo"
+      >
+      <h2>
+        <span>Buku</span>
+        <span class="text-success"> Ijo</span>
+      </h2>
+    </div>
+    <p class="subtitle text-secondary">
+      Ambil Nomor Ga Pake Ribet
+    </p>
+    <!-- LoginID -->
+    <label
+      for="loginid"
+      class="form-label"
+    >NIP Pendek</label>
+    <input
+      id="loginid"
+      type="text"
+      name="loginid"
+      class="form-control mb-4"
+    >
+    <!-- Password -->
+    <label
+      for="password"
+      class="form-label"
+    >Password</label>
+    <input
+      id="password"
+      type="password"
+      name="password"
+      class="form-control mb-4"
+    >
+    <!-- CTA -->
+    <button class="btn btn-success">
+      Masuk Gan
+    </button>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.logo {
+  width: 2.25rem;
+  margin-right: 0.5rem;
+}
+
+h2 {
+  margin-bottom: 0;
+  font-weight: bold;
+}
+
+.subtitle {
+  font-size: 0.8rem;
+  margin-top: 0.25rem;
+  margin-bottom: 2.5rem;
+}
+
+button {
+  font-weight: 600;
+}
+</style>
