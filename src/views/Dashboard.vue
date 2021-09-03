3ae355ba59662fbe666f11b491901ec41aa46e40
Zulham Syafrawi <official.zulham@gmail.com>
2021-09-03 09:05:42 +0700

    add surat section done

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,69 @@
 <template>
-  <TheNavbar />
+  <TheNavbar class="mb-3" />
+  <main class="container">
+    <!-- Title -->
+    <p class="lead">
+      Buat Surat Baru
+    </p>
+    <!-- Form -->
+    <form>
+      <!-- Jenis Surat -->
+      <label
+        class="form-label"
+        for="jenissurat"
+      >Jenis Surat</label>
+      <select
+        id="jenissurat"
+        class="form-select mb-3"
+        aria-label="Default select example"
+      >
+        <option value="ND">
+          Nota Dinas
+        </option>
+        <option value="S">
+          Surat
+        </option>
+      </select>
+      <!-- Perihal -->
+      <label
+        for="perihal"
+        class="form-label"
+      >Perihal</label>
+      <input
+        id="perihal"
+        type="text"
+        name="perihal"
+        class="form-control mb-3"
+      >
+      <div class="d-flex justify-content-between mb-4">
+        <!-- DIV / Tanggal dan Tanggal Terakhir -->
+        <div>
+          <label
+            for="tanggal"
+            class="form-label"
+          >Tanggal</label>
+          <input
+            id="tanggal"
+            type="date"
+            name="tanggal"
+            class="form-control"
+            min="2021-09-01"
+          >
+        </div>
+        <!-- Info Tanggal Surat Terakhir -->
+        <div>
+          <label class="form-label">Tanggal Surat Terakhir</label>
+          <p class="mb-0 py-1">
+            28 Agustus 2021
+          </p>
+        </div>
+      </div>
+      <!-- CTA -->
+      <button class="btn btn-success w-100">
+        Ambil Nomor
+      </button>
+    </form>
+  </main>
 </template>
 
 <script>
@@ -11,5 +75,17 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.lead {
+  font-weight: 600;
+}
+
+label {
+  opacity: 55%;
+  font-weight: 600;
+}
+
+button {
+  font-weight: 600;
+}
 </style>
