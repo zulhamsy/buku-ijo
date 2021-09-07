df26a1aa52f40e3e09d85db7ee8d3aeb87bd1ec0
Zulham Syafrawi <official.zulham@gmail.com>
2021-09-07 10:15:42 +0700

    fixing min max bug at some date

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -205,11 +205,10 @@ export default {
     minmax(date) {
       const year = date.getFullYear()
       const month =
-        (date.getMonth() + 1).length != 1
+        date.getMonth() + 1 < 10
           ? `0${date.getMonth() + 1}`
           : date.getMonth() + 1
-      const day =
-        date.getDate().length != 1 ? `0${date.getDate()}` : date.getDate()
+      const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate()
       return `${year}-${month}-${day}`
     },
     async inputSurat() {
