91d481a7c19d0b6a8b4133357142bc57e24a8cfe
Zulham Syafrawi <official.zulham@gmail.com>
2021-09-20 16:08:26 +0700

    fixing inconsitent date bug

--- src/store/index.js
+++ src/store/index.js
@@ -71,6 +71,8 @@ const store = createStore({
           const lastDocRef = doc(suratDB, '--stats--')
           const lastDoc = await transaction.get(lastDocRef)
           let nomor_surat = lastDoc.get(`nomor_terakhir.${payload.jenis_surat}`)
+          let tanggal_terakhir_nd = lastDoc.get('tanggal_terakhir.ND')
+          let tanggal_terakhir_s = lastDoc.get('tanggal_terakhir.S')
           const tahunTerakhir = lastDoc
             .get(`tanggal_terakhir.${payload.jenis_surat}`)
             .toDate()
@@ -79,6 +81,12 @@ const store = createStore({
             nomor_surat = 1
           } else {
             nomor_surat++
+          }
+          // cek tanggal mana yang perlu diupdate
+          if (payload.jenis_surat == 'ND') {
+            tanggal_terakhir_nd = payload.tanggal_surat
+          } else {
+            tanggal_terakhir_s = payload.tanggal_surat
           }
           // write document
           transaction.set(doc(suratDB), {
@@ -91,8 +99,10 @@ const store = createStore({
             lastDocRef,
             `nomor_terakhir.${payload.jenis_surat}`,
             nomor_surat,
-            `tanggal_terakhir.${payload.jenis_surat}`,
-            payload.tanggal_surat
+            'tanggal_terakhir.ND',
+            tanggal_terakhir_nd,
+            'tanggal_terakhir.S',
+            tanggal_terakhir_s
           )
           nomor_surat_ditambahkan = nomor_surat
         })
