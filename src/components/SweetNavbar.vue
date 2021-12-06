94c044193e68f4cb6fa0ff4663b9124d82f6025a
Zulham Syafrawi <official.zulham@gmail.com>
2021-12-06 16:53:32 +0700

    change logout button icon to option icon

--- src/components/SweetNavbar.vue
+++ src/components/SweetNavbar.vue
@@ -17,9 +17,7 @@ export default {
 <template>
   <div class="navbar mb-2 shadow-lg bg-neutral text-neutral-content">
     <div class="flex-1 px-2 mx-2 gap-x-2">
-      <span class="text-lg font-thin">
-        Halo,
-      </span>
+      <span class="text-lg font-thin"> Halo, </span>
       <span class="text-lg font-semibold">
         {{ username }}
       </span>
@@ -27,11 +25,11 @@ export default {
     <!-- Logout hehe -->
     <div class="flex-none">
       <button
-        class="btn btn-square btn-ghost focus:bg-gray-600"
+        class="btn btn-square btn-ghost focus:bg-gray-600 hover:bg-gray-500"
         @click="logout()"
       >
         <img
-          src="../assets/logout.svg"
+          src="../assets/vertical_dots.svg"
           alt="logout"
         >
       </button>
